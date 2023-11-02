06b07bedd001225362d8197ab5ca7607e7b0010d
SUJITH S <113872741+sujithdsacoder@users.noreply.github.com>
2023-11-02 09:44:31 +0530

    Create Kadane’s Algorithm Brute Force Approach.py

---
+++ Kadane’s Algorithm Brute Force Approach.py
@@ -0,0 +1,23 @@
+import sys
+
+def maxSubarraySum(arr, n):
+    maxi = -sys.maxsize - 1  # maximum sum
+
+    for i in range(n):
+        for j in range(i, n):
+            # subarray = arr[i.....j]
+            summ = 0
+
+            # add all the elements of subarray:
+            for k in range(i, j+1):
+                summ += arr[k]
+
+            maxi = max(maxi, summ)
+
+    return maxi
+
+if __name__ == "__main__":
+    arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    n = len(arr)
+    maxSum = maxSubarraySum(arr, n)
+    print("The maximum subarray sum is:", maxSum)
