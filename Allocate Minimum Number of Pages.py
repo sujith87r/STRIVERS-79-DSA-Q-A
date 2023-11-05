1e5e4384f4754521f6fd80893d46276703b8d9b4
SUJITH S <113872741+sujithdsacoder@users.noreply.github.com>
2023-11-05 21:38:27 +0530

    Create Allocate Minimum Number of Pages.py

---
+++ Allocate Minimum Number of Pages.py
@@ -0,0 +1,33 @@
+def countStudents(arr, pages):
+    n = len(arr)  # size of array
+    students = 1
+    pagesStudent = 0
+    for i in range(n):
+        if pagesStudent + arr[i] <= pages:
+            # add pages to current student
+            pagesStudent += arr[i]
+        else:
+            # add pages to next student
+            students += 1
+            pagesStudent = arr[i]
+    return students
+
+def findPages(arr, n, m):
+    # book allocation impossible
+    if m > n:
+        return -1
+
+    low = max(arr)
+    high = sum(arr)
+
+    for pages in range(low, high + 1):
+        if countStudents(arr, pages) == m:
+            return pages
+    return low
+
+arr = [25, 46, 28, 49, 24]
+n = 5
+m = 4
+ans = findPages(arr, n, m)
+print("The answer is:", ans)
+
