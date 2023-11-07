bd30568d9e2c80e5e87e3a7223f03e026c56c7a1
SUJITH S <113872741+sujithdsacoder@users.noreply.github.com>
2023-11-07 09:16:00 +0530

    Create Remove N-th node from the end of a Linked List.py

---
+++ Remove N-th node from the end of a Linked List.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        start = ListNode()
+        start.next = head
+        fast = start
+        slow = start
+
+
+        for i in range(1, n+1):
+            fast = fast.next
+
+
+        while fast.next != None:
+            fast = fast.next
+            slow = slow.next
+
+
+        slow.next = slow.next.next
+
+
+        return start.next
